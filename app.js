6b50407ce6c356c9dcf000f637943a53da1f807a
adewale shelby <musiliu.adewale@yahoo.com>
2020-12-24 16:34:16 -0800

    Added my project

---
+++ app.js
@@ -0,0 +1,11 @@
+const deleted = document.querySelector('.delete');
+const firstTodo = document.querySelector('.firstTodo');
+const checked1 = document.querySelector('.checkbox');
+
+deleted.addEventListener('click', function(){
+    firstTodo.remove('.firstTodo')
+})
+
+checked1.addEventListener('click', function(){
+    
+})
